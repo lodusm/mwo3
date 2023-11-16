da19fe04f08b53d27eb64f357b9b2af5325173d6
Maciej Sudoł <lodus@Maciejs-MacBook-Air.local>
2023-11-16 09:36:59 +0100

    bbb

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -3,4 +3,5 @@ public class App
     public static void main( String[] args ){
     Calc calc = new Calc();
     System.out.println(calc.average(2,4));
+    System.out.println("odp: " + calc.isEven(6));
 }}
