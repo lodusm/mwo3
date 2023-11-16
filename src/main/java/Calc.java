a2e74bbf18244ba541431002d8cbdf5069ef6cc6
Maciej Sudoł <lodus@Maciejs-MacBook-Air.local>
2023-11-16 09:25:30 +0100

    ve

---
+++ src/main/java/Calc.java
@@ -0,0 +1,15 @@
+public class Calc {
+    double average(double a, double b){
+        return (a+b)/2;
+    }
+
+    boolean isEven(int a){
+        if (a%2==0) return true;
+        else return false;
+    }
+
+    boolean isNotEven(int a){
+        if (a%2==1) return true;
+        else return false;
+    }
+}
